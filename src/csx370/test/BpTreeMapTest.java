eca4abaea680947e79f89d269bf2669900af5066
vincentclee <vincentlee.dolbydigital@yahoo.com>
2014-06-27 11:25:28 -0500

    Combined BpTreeMapTest with BpTreeTest

--- src/csx370/test/BpTreeMapTest.java
+++ src/csx370/test/BpTreeMapTest.java
@@ -98,4 +98,108 @@ public class BpTreeMapTest {
 	public void subMapTest() {
 		assertEquals("subMap", treeMap.subMap(treeMap.firstKey(), treeMap.lastKey()).toString(), bpt.subMap(treeMap.firstKey(), treeMap.lastKey()).toString());
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@Test
+	public void sizeTest()
+	{
+		BpTreeMap<Integer, Integer> bptest = new BpTreeMap<>(Integer.class, Integer.class);
+		int totKeys = 12;
+
+		for (int i = 1; i < totKeys; i += 2)
+		{
+			bptest.put(i, i * i);
+		}
+		int TestSize = bptest.size();
+		org.junit.Assert.assertEquals(" The size is ", TestSize, 6);
+	}
+
+	@Test
+	public void subMapTest1()
+	{
+		BpTreeMap<Integer, Integer> bptest = new BpTreeMap<>(Integer.class, Integer.class);
+		int totKeys = 12;
+
+		for (int i = 1; i < totKeys; i += 2)
+		{
+			bptest.put(i, i * i);
+		}
+
+
+		SortedMap<Integer, Integer> SubMapTest = (SortedMap<Integer, Integer>) bptest.subMap(3, 9);
+		String  st = SubMapTest.toString();
+
+		org.junit.Assert.assertEquals("SubMap Test is ", st, "{3=9, 5=25, 7=49}");
+
+	}
+	@Test
+	public void headMapTest1()
+	{
+		BpTreeMap<Integer, Integer> bptest = new BpTreeMap<>(Integer.class, Integer.class);
+		int totKeys = 12;
+
+		for (int i = 1; i < totKeys; i += 2)
+		{
+			bptest.put(i, i * i);
+		}
+
+
+		SortedMap<Integer, Integer> HeadTest = (SortedMap<Integer, Integer>) bptest.headMap(5);
+		String  st =HeadTest.toString();
+
+		org.junit.Assert.assertEquals("HeadMap Test is ", st, "{1=1, 3=9}");
+	}
+	@Test
+	public void tailMapTest1()
+	{
+		BpTreeMap<Integer, Integer> bptest = new BpTreeMap<>(Integer.class, Integer.class);
+		int totKeys = 12;
+
+		for (int i = 1; i < totKeys; i += 2)
+		{
+			bptest.put(i, i * i);
+		}
+
+
+		SortedMap<Integer, Integer> TailTest = (SortedMap<Integer, Integer>) bptest.tailMap(5);
+		String  st =TailTest.toString();
+
+		org.junit.Assert.assertEquals("TailMap Test is ", st, "{5=25, 7=49, 9=81, 11=121}");
+	}
+	@Test
+
+	public void firstKeyTest()
+	{
+		BpTreeMap<Integer, Integer> bptest = new BpTreeMap<>(Integer.class, Integer.class);
+		int totKeys = 12;
+
+		for (int i = 1; i < totKeys; i += 2)
+		{
+			bptest.put(i, i * i);
+		}
+		int TestFirst = bptest.firstKey();
+		org.junit.Assert.assertEquals(" First Key is ", TestFirst, 1);
+
+	}
+	@Test
+	public void lastKeyTest()
+	{
+		BpTreeMap<Integer, Integer> bptest = new BpTreeMap<>(Integer.class, Integer.class);
+		int totKeys = 12;
+
+		for (int i = 1; i < totKeys; i += 2)
+		{
+			bptest.put(i, i * i);
+		}
+		int TestLast = bptest.lastKey();
+		org.junit.Assert.assertEquals(" First Key is ", TestLast, 11);
+	}
 }
